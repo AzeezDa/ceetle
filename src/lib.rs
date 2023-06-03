60de5b9ef7b02bbbf58443a076a97190d99d6a99
AzeezDaoud <azeez.daoud3@gmail.com>
2023-06-03 13:24:44 +0200

    Make discrete models easier to make

--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,16 @@ mod tests {
     use std::{collections::HashMap, vec};
 
     use super::*;
-    use crate::model::{HashedDiscreteModel, Model, DiscreteModel, verify, CTLFormula};
+    use crate::model::{verify, CTLFormula, DiscreteModel, HashedDiscreteModel, Model};
 
     #[test]
     fn create_and_check_discrete_model() {
-        let model = DiscreteModel::new(
-            vec![
-                ("s0", vec!["a"]),
-                ("s1", vec!["a", "b"]),
-                ("s2", vec!["a"]),
-                ("s3", vec!["a", "c"]),
-            ],
-            vec![
-                ("s0", vec!["s0", "s1"]),
-                ("s1", vec!["s0", "s2"]),
-                ("s2", vec!["s3"]),
-                ("s3", vec!["s1", "s3"]),
-            ],
-        );
+        let model = DiscreteModel::new(vec![
+            ("s0", vec!["a"], vec!["s0", "s1"]),
+            ("s1", vec!["a", "b"], vec!["s0", "s2"]),
+            ("s2", vec!["a"], vec!["s3"]),
+            ("s3", vec!["a", "c"], vec!["s1", "s3"]),
+        ]);
 
         assert!(model.state_has(&"s0", &"a"));
         assert!(!model.state_has(&"s3", &"b"));
@@ -32,19 +24,13 @@ mod tests {
 
     #[test]
     fn create_and_check_hashed_discrete_model() {
-        let mut labels = HashMap::new();
-        labels.insert("s0", vec!["a"]);
-        labels.insert("s1", vec!["a", "b"]);
-        labels.insert("s2", vec!["a"]);
-        labels.insert("s3", vec!["a", "c"]);
+        let mut states = HashMap::new();
+        states.insert("s0", (vec!["a"], vec!["s0", "s1"]));
+        states.insert("s1", (vec!["a", "b"], vec!["s0", "s2"]));
+        states.insert("s2", (vec!["a"], vec!["s3"]));
+        states.insert("s3", (vec!["a", "c"], vec!["s1", "s3"]));
 
-        let mut transitions = HashMap::new();
-        transitions.insert("s0", vec!["s0", "s1"]);
-        transitions.insert("s1", vec!["s0", "s2"]);
-        transitions.insert("s2", vec!["s3"]);
-        transitions.insert("s3", vec!["s1", "s3"]);
-
-        let model = HashedDiscreteModel::new(labels, transitions);
+        let model = HashedDiscreteModel::new(states);
         assert!(model.state_has(&"s0", &"a"));
         assert!(!model.state_has(&"s3", &"b"));
         assert_eq!(*model.transitions(&"s1"), vec!["s0", "s2"]);
@@ -52,108 +38,84 @@ mod tests {
 
     #[test]
     fn check_ex() {
-        let model = DiscreteModel::new(
-            vec![
-                ("s0", vec![]),
-                ("s1", vec!["p"]),
-                ("s2", vec!["q"]),
-                ("s3", vec!["p", "q"]),
-            ],
-            vec![
-                ("s0", vec!["s1", "s2"]),
-                ("s1", vec!["s1", "s2", "s3"]),
-                ("s2", vec!["s1"]),
-                ("s3", vec!["s0"]),
-            ]
-        );
+        let model = DiscreteModel::new(vec![
+            ("s0", vec![], vec!["s1", "s2"]),
+            ("s1", vec!["p"], vec!["s1", "s2", "s3"]),
+            ("s2", vec!["q"], vec!["s1"]),
+            ("s3", vec!["p", "q"], vec!["s0"]),
+        ]);
 
         assert!(verify(&model, &"s0", &ctl!(EX(Atom("p")))));
         assert!(!verify(&model, &"s0", &ctl!(EX(And(Atom("p"), Atom("q"))))));
-        assert!(verify(&model, &"s0", &ctl!(EX(EX(And(Atom("p"), Atom("q")))))));
+        assert!(verify(
+            &model,
+            &"s0",
+            &ctl!(EX(EX(And(Atom("p"), Atom("q")))))
+        ));
     }
 
     #[test]
     fn check_ax() {
-        let model = DiscreteModel::new(
-            vec![
-                ("s0", vec![]),
-                ("s1", vec!["p"]),
-                ("s2", vec!["q"]),
-                ("s3", vec!["p", "q"]),
-            ],
-            vec![
-                ("s0", vec!["s1", "s2"]),
-                ("s1", vec!["s1", "s2", "s3"]),
-                ("s2", vec!["s1"]),
-                ("s3", vec!["s0"]),
-            ]
-        );
+        let model = DiscreteModel::new(vec![
+            ("s0", vec![], vec!["s1", "s2"]),
+            ("s1", vec!["p"], vec!["s1", "s2", "s3"]),
+            ("s2", vec!["q"], vec!["s1"]),
+            ("s3", vec!["p", "q"], vec!["s0"]),
+        ]);
 
         assert!(verify(&model, &"s0", &ctl!(AX(Or(Atom("p"), Atom("q"))))));
-        assert!(!verify(&model, &"s0", &ctl!(AX(AX(AX(Or(Atom("p"), Atom("q"))))))));
-        assert!(verify(&model, &"s0", &ctl!(AX(Imply(Atom("p"), Not(Atom("q")))))));
+        assert!(!verify(
+            &model,
+            &"s0",
+            &ctl!(AX(AX(AX(Or(Atom("p"), Atom("q"))))))
+        ));
+        assert!(verify(
+            &model,
+            &"s0",
+            &ctl!(AX(Imply(Atom("p"), Not(Atom("q")))))
+        ));
     }
 
     #[test]
     fn check_eg() {
-        let model = DiscreteModel::new(
-            vec![
-                ("s0", vec!["p"]),
-                ("s1", vec!["p"]),
-                ("s2", vec![]),
-                ("s3", vec!["p", "q"]),
-            ],
-            vec![
-                ("s0", vec!["s1"]),
-                ("s1", vec!["s2", "s3"]),
-                ("s2", vec!["s0", "s3"]),
-                ("s3", vec!["s3"]),
-            ]
-        );
+        let model = DiscreteModel::new(vec![
+            ("s0", vec!["p"], vec!["s1"]),
+            ("s1", vec!["p"], vec!["s2", "s3"]),
+            ("s2", vec![], vec!["s0", "s3"]),
+            ("s3", vec!["p", "q"], vec!["s3"]),
+        ]);
 
         assert!(verify(&model, &"s0", &ctl!(EG(Atom("p")))));
         assert!(verify(&model, &"s0", &ctl!(EG(Not(Atom("q"))))));
-        assert!(!verify(&model, &"s0", &ctl!(EG(And(Atom("p"),Atom("q"))))));
+        assert!(!verify(&model, &"s0", &ctl!(EG(And(Atom("p"), Atom("q"))))));
     }
 
     #[test]
     fn check_ag() {
-        let model = DiscreteModel::new(
-            vec![
-                ("s0", vec!["p"]),
-                ("s1", vec!["p"]),
-                ("s2", vec![]),
-                ("s3", vec!["p", "q"]),
-            ],
-            vec![
-                ("s0", vec!["s1"]),
-                ("s1", vec!["s2", "s3"]),
-                ("s2", vec!["s0", "s3"]),
-                ("s3", vec!["s3"]),
-            ]
-        );
+        let model = DiscreteModel::new(vec![
+            ("s0", vec!["p"], vec!["s1"]),
+            ("s1", vec!["p"], vec!["s2", "s3"]),
+            ("s2", vec![], vec!["s0", "s3"]),
+            ("s3", vec!["p", "q"], vec!["s3"]),
+        ]);
 
         assert!(!verify(&model, &"s0", &ctl!(AG(Atom("p")))));
-        assert!(verify(&model, &"s0", &ctl!(AG(Imply(Atom("q"), Atom("p"))))));
+        assert!(verify(
+            &model,
+            &"s0",
+            &ctl!(AG(Imply(Atom("q"), Atom("p"))))
+        ));
         assert!(verify(&model, &"s0", &ctl!(AG(EX(Atom("p"))))));
     }
 
     #[test]
     fn check_ef() {
-        let model = DiscreteModel::new(
-            vec![
-                ("s0", vec!["p"]),
-                ("s1", vec!["q"]),
-                ("s2", vec!["p", "r"]),
-                ("s3", vec!["q", "r"]),
-            ],
-            vec![
-                ("s0", vec!["s1"]),
-                ("s1", vec!["s0", "s3"]),
-                ("s2", vec!["s3"]),
-                ("s3", vec!["s2"]),
-            ]
-        );
+        let model = DiscreteModel::new(vec![
+            ("s0", vec!["p"], vec!["s1"]),
+            ("s1", vec!["q"], vec!["s0", "s3"]),
+            ("s2", vec!["p", "r"], vec!["s3"]),
+            ("s3", vec!["q", "r"], vec!["s2"]),
+        ]);
 
         assert!(verify(&model, &"s0", &ctl!(EF(AG(Atom("r"))))));
         assert!(verify(&model, &"s0", &ctl!(EF(AX(Atom("r"))))));
@@ -162,67 +124,59 @@ mod tests {
 
     #[test]
     fn check_af() {
-        let model = DiscreteModel::new(
-            vec![
-                ("s0", vec!["p"]),
-                ("s1", vec!["p"]),
-                ("s2", vec!["p", "r"]),
-                ("s3", vec!["q", "r"]),
-            ],
-            vec![
-                ("s0", vec!["s1"]),
-                ("s1", vec!["s0", "s3"]),
-                ("s2", vec!["s3"]),
-                ("s3", vec!["s2"]),
-            ]
-        );
+        let model = DiscreteModel::new(vec![
+            ("s0", vec!["p"], vec!["s1"]),
+            ("s1", vec!["p"], vec!["s0", "s3"]),
+            ("s2", vec!["p", "r"], vec!["s3"]),
+            ("s3", vec!["q", "r"], vec!["s2"]),
+        ]);
 
         assert!(!verify(&model, &"s0", &ctl!(AF(Atom("r")))));
-        assert!(verify(&model, &"s0", &ctl!(AF(Or(EG(Atom("p")), EG(Atom("r")))))));
-        assert!(verify(&model, &"s0", &ctl!(AF(Imply(Atom("r"), Or(Atom("p"), Atom("q")))))));
+        assert!(verify(
+            &model,
+            &"s0",
+            &ctl!(AF(Or(EG(Atom("p")), EG(Atom("r")))))
+        ));
+        assert!(verify(
+            &model,
+            &"s0",
+            &ctl!(AF(Imply(Atom("r"), Or(Atom("p"), Atom("q")))))
+        ));
     }
 
     #[test]
     fn check_eu() {
-        let model = DiscreteModel::new(
-            vec![
-                ("s0", vec!["p", "r"]),
-                ("s1", vec![]),
-                ("s2", vec!["p"]),
-                ("s3", vec!["p", "q"]),
-            ],
-            vec![
-                ("s0", vec!["s1", "s2"]),
-                ("s1", vec!["s3"]),
-                ("s2", vec!["s2", "s3"]),
-                ("s3", vec!["s0"]),
-            ]
-        );
+        let model = DiscreteModel::new(vec![
+            ("s0", vec!["p", "r"], vec!["s1", "s2"]),
+            ("s1", vec![], vec!["s3"]),
+            ("s2", vec!["p"], vec!["s2", "s3"]),
+            ("s3", vec!["p", "q"], vec!["s0"]),
+        ]);
 
         assert!(verify(&model, &"s0", &ctl!(EU(Atom("p"), Atom("q")))));
         assert!(verify(&model, &"s0", &ctl!(EU(Atom("p"), EG(Atom("p"))))));
-        assert!(!verify(&model, &"s0", &ctl!(EU(And(Atom("p"), Atom("r")), Atom("q")))));
+        assert!(!verify(
+            &model,
+            &"s0",
+            &ctl!(EU(And(Atom("p"), Atom("r")), Atom("q")))
+        ));
     }
 
     #[test]
     fn check_au() {
-        let model = DiscreteModel::new(
-            vec![
-                ("s0", vec!["p"]),
-                ("s1", vec!["p", "r"]),
-                ("s2", vec!["q"]),
-                ("s3", vec!["p", "q", "r"]),
-            ],
-            vec![
-                ("s0", vec!["s1"]),
-                ("s1", vec!["s2"]),
-                ("s2", vec!["s2", "s3"]),
-                ("s3", vec!["s1"]),
-            ]
-        );
+        let model = DiscreteModel::new(vec![
+            ("s0", vec!["p"], vec!["s1"]),
+            ("s1", vec!["p", "r"], vec!["s2"]),
+            ("s2", vec!["q"], vec!["s2", "s3"]),
+            ("s3", vec!["p", "q", "r"], vec!["s1"]),
+        ]);
 
         assert!(verify(&model, &"s0", &ctl!(AU(Atom("p"), Atom("q")))));
-        assert!(verify(&model, &"s0", &ctl!(AU(Atom("p"), AG(Or(Atom("r"), Atom("q")))))));
+        assert!(verify(
+            &model,
+            &"s0",
+            &ctl!(AU(Atom("p"), AG(Or(Atom("r"), Atom("q")))))
+        ));
         assert!(!verify(&model, &"s0", &ctl!(AU(Atom("p"), Atom("r")))));
     }
 }
--- src/model/model.rs
+++ src/model/model.rs
@@ -6,33 +6,49 @@ use std::{collections::HashMap, hash::Hash};
 /// - The `state_has` function is the membership function that takes a state in the model and an atom and returns true if the atom exists in the state
 /// - The `transitions` function is the transitions function that takes a state and returns a `&Vec` of the possible transitions from that state
 /// 
-/// The model is parametrised by two types `S` and `L`. `S` is the type of state, usually states are be labeled by a string (`s0`, `s1`, etc.), so a `&str` could work here. 
-/// The `L` type is the type of atoms used in the model, usually a string or char (`p`, `q`, etc.) is used for `L` as well.
-pub trait Model<S, L> {
-    fn state_has(&self, state: &S, atom: &L) -> bool;
+/// The model is parametrised by two types `S` and `A`. `S` is the type of state, usually states are be labeled by a string (`s0`, `s1`, etc.), so a `&str` could work here. 
+/// The `A` type is the type of atoms used in the model, usually a string or char (`p`, `q`, etc.) is used for `A` as well.
+pub trait Model<S, A> {
+    fn state_has(&self, state: &S, atom: &A) -> bool;
     fn transitions(&self, state: &S) -> &Vec<S>;
 }
 
 /// # `DiscreteModel`
 /// The `DiscreteModel` is identical to a finite-state automaton. It uses a `Vec` to store each state together with its atoms and transitions. 
 /// Therefore, lookup is `O(n)`, where `n` is the number of states.
-pub struct DiscreteModel<S, L> {
-    labels: Vec<(S, Vec<L>)>,
-    transitions: Vec<(S, Vec<S>)>,
+pub struct DiscreteModel<S, A> {
+    states: Vec<(S, Vec<A>, Vec<S>)>,
 }
 
-impl<S: PartialEq, L: PartialEq> DiscreteModel<S, L> {
-    pub fn new(labels: Vec<(S, Vec<L>)>, transitions: Vec<(S, Vec<S>)>) -> Self {
+impl<S: PartialEq, A: PartialEq> DiscreteModel<S, A> {
+    /// # `new`
+    /// Returns a new `DiscreteModel` with a structure given as a `Vec` of tuples `(S, Vec<A>, Vec<S>)`, where
+    /// - The first value is the state label
+    /// - The second value is a `Vec` of atoms that hold in that state
+    /// - The last value is a `Vec` of states that can be transitioned to from this state
+    /// 
+    /// ## Examples
+    /// ```
+    /// use ceetle::DiscreteModel;
+    /// 
+    /// let model = DiscreteModel::new(vec![
+    ///     ("s0", vec!["a"], vec!["s0", "s1"]),
+    ///     ("s1", vec!["a", "b"], vec!["s0"])
+    /// ]);
+    /// ```
+    pub fn new(states: Vec<(S, Vec<A>, Vec<S>)>) -> Self {
         Self {
-            labels,
-            transitions,
+            states,
         }
     }
 }
 
-impl<S: PartialEq, L: PartialEq> Model<S, L> for DiscreteModel<S, L> {
-    fn state_has(&self, state: &S, atom: &L) -> bool {
-        for s in self.labels.iter() {
+impl<S: PartialEq, A: PartialEq> Model<S, A> for DiscreteModel<S, A> {
+
+    /// # `state_has`
+    /// Takes a state and an atom and returns true if the atom holds in that state. This operation is `O(n)`, where `n` is the number of states.
+    fn state_has(&self, state: &S, atom: &A) -> bool {
+        for s in self.states.iter() {
             if s.0 == *state {
                 return s.1.contains(&atom);
             }
@@ -40,10 +56,12 @@ impl<S: PartialEq, L: PartialEq> Model<S, L> for DiscreteModel<S, L> {
         panic!("State is not in model!");
     }
 
+    /// # `transitions`
+    /// Takes a state and returns a `&Vec` of the states that the given state can transition to. This operation is `O(n)`, where `n` is the number of states.
     fn transitions(&self, state: &S) -> &Vec<S> {
-        for s in self.transitions.iter() {
+        for s in self.states.iter() {
             if s.0 == *state {
-                return &s.1;
+                return &s.2;
             }
         }
         panic!("State is not in model!");
@@ -53,31 +71,53 @@ impl<S: PartialEq, L: PartialEq> Model<S, L> for DiscreteModel<S, L> {
 /// # `HashedDiscreteModel`
 /// The `HashedDiscreteModel` is identical to a finite-state automaton. It uses a `HashMap` to store each state together with its atoms and transitions. 
 /// Therefore, lookup is `O(1)`.
-pub struct HashedDiscreteModel<S, L> {
-    labels: HashMap<S, Vec<L>>,
-    transitions: HashMap<S, Vec<S>>,
+pub struct HashedDiscreteModel<S, A> {
+    states: HashMap<S, (Vec<A>, Vec<S>)>,
 }
 
-impl<S: PartialEq, L: PartialEq> HashedDiscreteModel<S, L> {
-    pub fn new(labels: HashMap<S, Vec<L>>, transitions: HashMap<S, Vec<S>>) -> Self {
+impl<S: PartialEq, A: PartialEq> HashedDiscreteModel<S, A> {
+    /// # `new`
+    /// Returns a new `HashedDiscreteModel` with a structure given as a `HashMap` of keys of type `S` and values of type `(Vec<A>, Vec<S>)`, where
+    /// - The key is the state label
+    /// - The first of the value's tuple is a `Vec` of atoms that hold in that state
+    /// - The second of the value's tuple is a `Vec` of states that can be transitioned to from this state
+    /// 
+    /// ## Examples
+    /// ```
+    /// use std::collections::HashMap;
+    /// use ceetle::HashedDiscreteModel;
+    /// 
+    /// let mut states = HashMap::new();
+    /// states.insert("s0", (vec!["a"], vec!["s0", "s1"]));
+    /// states.insert("s1", (vec!["a", "b"], vec!["s0", "s2"]));
+    /// states.insert("s2", (vec!["a"], vec!["s3"]));
+    /// states.insert("s3", (vec!["a", "c"], vec!["s1", "s3"]));
+    /// 
+    /// let model = HashedDiscreteModel::new(states);
+    /// ```
+    pub fn new(states: HashMap<S, (Vec<A>, Vec<S>)>) -> Self {
         Self {
-            labels,
-            transitions,
+            states
         }
     }
 }
 
-impl<S: Eq + Hash, L: PartialEq> Model<S, L> for HashedDiscreteModel<S, L> {
-    fn state_has(&self, state: &S, atom: &L) -> bool {
-        if let Some(atoms) = self.labels.get(&state) {
-            return atoms.contains(&atom);
+impl<S: Eq + Hash, A: PartialEq> Model<S, A> for HashedDiscreteModel<S, A> {
+
+    /// # `state_has`
+    /// Takes a state and an atom and returns true if the atom holds in that state. This operation is `O(1)`.
+    fn state_has(&self, state: &S, atom: &A) -> bool {
+        if let Some(atoms) = self.states.get(&state) {
+            return atoms.0.contains(&atom);
         }
         panic!("State is not in model!");
     }
 
+    /// # `transitions`
+    /// Takes a state and returns a `&Vec` of the states that the given state can transition to. This operation is `O(1)`.
     fn transitions(&self, state: &S) -> &Vec<S> {
-        if let Some(nexts) = self.transitions.get(&state) {
-            return nexts;
+        if let Some(nexts) = self.states.get(&state) {
+            return &(*nexts).1;
         }
         panic!("State is not in model!");
     }
